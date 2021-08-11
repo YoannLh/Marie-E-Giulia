d65eb00fecc22d92349d324cbe43ed5289bff0c7
Yoann L'hostis <yolhostis@live.fr>
2021-08-11 18:52:21 +0200

    Ajout feature telephone et adresse dans footer

---
+++ js/features.js
@@ -0,0 +1,20 @@
+
+let iconeTel = document.getElementById("telephoneIsNotVisible");
+let numberTel = document.getElementById("visibleTelephoneOrNot");
+let mail = document.getElementById("iconeMail");
+
+if (window.matchMedia("(min-width: 768px)").matches) {
+  	iconeTel.addEventListener("click", () => {
+  	iconeTel.style.display = "none";
+  	numberTel.style.display = "block";
+  	mail.style.marginLeft = "50%";
+  })
+  numberTel.addEventListener("click", () => {
+  	numberTel.style.display = "none";
+  	iconeTel.style.display = "block";
+  	mail.style.marginLeft = "0";
+  })
+}
+
+
+
